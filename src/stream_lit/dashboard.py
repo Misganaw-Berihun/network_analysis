f06b3a408238c5033cb2c42282f65ed8b94b0fef
Misganaw-Berihun <misganawbmb@gmail.com>
2023-12-01 21:34:49 +0300

    fix: fix dashboard layout

--- src/stream_lit/dashboard.py
+++ src/stream_lit/dashboard.py
@@ -1,71 +1,66 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-
 import os
 import sys
 
-rpath = os.path.abspath('../..')
+rpath = os.path.abspath('../../')
 if rpath not in sys.path:
     sys.path.insert(0, rpath)
-
-from src.loader import SlackDataLoader
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
+from src.loader import SlackDataLoader  # Adjust the import based on your project structure
 
 def plot_top_users_with_highest_replies(data):
-    row_with_highest_replies = df.loc[df['reply_count'].idxmax()]
-    user_with_highest_replies = row_with_highest_replies['sender_name']
-    num_replies = row_with_highest_replies['reply_count']
+    sorted_data = data.sort_values(by='reply_count', ascending=False)
+    top_users = sorted_data.head(3)
 
-    # Streamlit app
-    st.title('Top User with the Highest Replies Plot')
+    st.title('Users with Highest Replies')
 
-    # Create a DataFrame for the bar chart
-    chart_data = pd.DataFrame({
-        'User': [user_with_highest_replies],
-        'Number of Replies': [num_replies]
-    })
+    fig, ax = plt.subplots(figsize=(15, 7.5))
+    ax.bar(top_users['sender_name'], top_users['reply_count'], color='blue')
 
-    # Bar chart in Streamlit
-    st.bar_chart(chart_data.set_index('User'))
+    ax.set_xlabel('User')
+    ax.set_ylabel('Number of Replies')
+
+    st.pyplot(fig)
 
 def draw_df(data):
-    st.title('usernames with highest replies')
+    st.title('Usernames with Highest Replies')
     users = data.sort_values(by='reply_count', ascending=False).head(10)
     users_replies = users[['sender_name', 'reply_count']]
+    
     st.table(users_replies.reset_index(drop=True))
 
 def draw_message_with_replies(data):
+    st.title('Messages with Highest Replies')
     df = data.sort_values(by='reply_count', ascending=False).head(10)
     channels_replies = df[['channel', 'reply_count']]
+    
     st.table(channels_replies.reset_index(drop=True))
 
 def draw_reply_per_user_per_channel(data):
+    st.title('Plot of Number of Replies per User per Channel')
     grouped_df = data.groupby(['channel', 'sender_name'])['reply_count'].sum().unstack()
-    grouped_df.plot(kind='bar', figsize=(15, 7.5), stacked=True)
-    
-    plt.figure(figsize=(15, 7.5))
-    grouped_df.plot(kind='bar', stacked=True)
 
-    plt.title('Reply Counts per User per Channel')
+    grouped_df = data.groupby(['channel', 'sender_name'])['reply_count'].sum().unstack()
+    sorted_df = grouped_df.sum(axis=1).sort_values(ascending=False)
+    top_7_channels = sorted_df.head(7)
+
+    fig, ax = plt.subplots(figsize=(15, 7.5))
+    top_7_channels.plot(kind='bar', ax=ax, stacked=True)
+
     plt.xlabel('Channel')
     plt.ylabel('Total Reply Count')
     plt.legend(title='Sender Name', bbox_to_anchor=(1, 1))
-    st.pyplot(plt)
+
+    st.pyplot(fig)
 
 if __name__ == '__main__':
-    sl =SlackDataLoader('../../data/anonymized/')
+    sl = SlackDataLoader('../../data/anonymized/')
     df = sl.create_dataframe()
-    col1, col2 = st.columns(2)
-    with col1:
-        plot_top_users_with_highest_replies(df)
-    with col2:
-        draw_df(df)
+
     
-    col1, col2 = st.columns(2)
-    with col1:
-        draw_message_with_replies(df)
-    with col2:
-        draw_reply_per_user_per_channel(df)
+    plot_top_users_with_highest_replies(df)
+    draw_df(df)
+
+    draw_message_with_replies(df)
+    draw_reply_per_user_per_channel(df)
