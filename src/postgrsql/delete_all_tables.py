dff4eea88d6fe2e62cabefdb0d173ad7682e9472
Misganaw-Berihun <misganawbmb@gmail.com>
2023-12-01 17:12:59 +0300

    feat: dashboard and backend

--- src/postgrsql/delete_all_tables.py
+++ src/postgrsql/delete_all_tables.py
@@ -12,22 +12,17 @@ if rpath not in sys.path:
 from src.postgrsql.postgresql_connect import create_database_engine
 from sqlalchemy import create_engine, MetaData
 
-# Replace 'your_database_url' with your actual database connection URL
 database_url = 'postgresql://username:password@localhost/your_database'
 engine = create_database_engine()
 
-# Create a metadata object
 metadata = MetaData()
 
-# Reflect existing tables and associate them with the engine
 metadata.reflect(bind=engine)
 
-# Drop all tables
 for table in reversed(metadata.sorted_tables):
     table.drop(bind=engine)
 
 connection = engine.connect()
-# Optional: Commit changes if you want to persist the changes immediately
 connection.commit()
 connection.close()
 
